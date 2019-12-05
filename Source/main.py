21ff458a22d5b6cbd8f11a539514aca778a3ab51
Stanislav Bondarenko <stassss2009@gmail.com>
2019-12-05 02:10:14 +0200

    Lab 3 v1.0

--- Source/main.py
+++ Source/main.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
-from PIL import Image, ImageFilter
 from abc import ABC, abstractmethod
 from typing import List
+from PIL import Image, ImageFilter, ImageEnhance
 
 
 class Manager:
@@ -14,9 +14,11 @@ class Manager:
         self.image_obj = Image.open("./" + str(file_name))
         self.image_obj.show()
 
-    def save_file(self):
+    def save_file(self, out_name):
         self.image_obj.show()
-        self.image_obj.save("./output.jpg")
+        if str(out_name)[-4] != '.':
+            out_name = str(out_name) + ".png"
+        self.image_obj.save("./" + str(out_name))
 
     def init_filter_chain(self):
         self.filter_chain = FilterGroup()
@@ -24,15 +26,36 @@ class Manager:
     def add_filter_to_chain(self, f_id, *args):
         if self.filter_chain is None:
             self.init_filter_chain()
-        if f_id == "blur":
-            self.filter_chain.add(FilterBlur(args))
+        if isinstance(args[0], list):
+            self.filter_chain.add(Filter(f_id, args[0]))
+        else:
+            self.filter_chain.add(Filter(f_id, args))
 
     def remove_filter_from_chain(self):
-        # self.filter_chain.remove()
-        pass
+        self.filter_chain.remove()
 
     def apply_filter_chain(self):
-        self.image_obj = self.filter_chain.apply(self.image_obj)
+        if self.filter_chain is not None:
+            self.image_obj = self.filter_chain.apply(self.image_obj)
+
+    def save_complex_filter(self):
+        if self.filter_chain is not None:
+            file = open("filter.txt", "w")
+            file.write(self.filter_chain.get_filter_list())
+            file.close()
+
+    def load_complex_filter(self, file_name):
+        file = open(file_name, "r")
+        operations = file.read().split("\n")
+        for oper_str in operations:
+            oper_list = oper_str.split()
+            if oper_list:
+                f_id = oper_list[0]
+                if len(oper_list) > 1:
+                    args = oper_list[1:]
+                else:
+                    args = None
+                self.add_filter_to_chain(f_id, args)
 
 
 class FilterInterface(ABC):
@@ -47,7 +70,7 @@ class FilterInterface(ABC):
     def add(self, component: FilterInterface) -> None:
         pass
 
-    def remove(self, component: FilterInterface) -> None:
+    def remove(self) -> None:
         pass
 
     def is_composite(self) -> bool:
@@ -57,24 +80,49 @@ class FilterInterface(ABC):
     def apply(self, image):
         pass
 
+    @abstractmethod
+    def get_filter_list(self):
+        pass
 
-class Filter(FilterInterface, ABC):
-    def __init__(self, *args):
-        print("Created Filter")
-        self.image = None
+
+class Filter(FilterInterface):
+    def __init__(self, f_id, args):
+        print("Created Filter-class")
         self.args = args
+        self.name = str(f_id)
 
+    def get_filter_list(self) -> str:
+        return str(self.name) + ' ' + str(' '.join(map(str, list(self.args))))
 
-class FilterBlur(Filter):
-    def apply(self, image):
-        self.image = image
-        self.image = self.image.transpose(Image.ROTATE_90).filter(ImageFilter.BLUR)
-        return self.image
+    def apply(self, image) -> Image:
+        if self.name == "blur":
+            return image.filter(ImageFilter.GaussianBlur(int(self.args[0])))
+        elif self.name == "boxblur":
+            return image.filter(ImageFilter.BoxBlur(int(self.args[0])))
+        elif self.name == "rotate":
+            return image.rotate(float(self.args[0]))
+        elif self.name == "resize":
+            return image.resize((int(self.args[0]), int(self.args[1])))
+        elif self.name == "flip":
+            if self.args[0] == "v":
+                return image.transpose(Image.FLIP_LEFT_RIGHT)
+            elif self.args[0] == "h":
+                return image.transpose(Image.FLIP_TOP_BOTTOM)
+        elif self.name == "color":
+            return ImageEnhance.Color(image).enhance(float(self.args[0]))
+        elif self.name == "contrast":
+            return ImageEnhance.Contrast(image).enhance(float(self.args[0]))
+        elif self.name == "brightness":
+            return ImageEnhance.Brightness(image).enhance(float(self.args[0]))
+        elif self.name == "sharpness":
+            return ImageEnhance.Sharpness(image).enhance(float(self.args[0]))
+        else:
+            return image
 
 
 class FilterGroup(FilterInterface):
     def __init__(self):
-        print("Created filter group")
+        print("Created filter group-class")
         self.image = None
         self._children: List[FilterInterface] = []
 
@@ -82,30 +130,59 @@ class FilterGroup(FilterInterface):
         self._children.append(component)
         component.parent = self
 
-    def remove(self, component: FilterInterface) -> None:
-        self._children.remove(component)
-        component.parent = None
+    def remove(self) -> None:
+        self._children[-1].parent = None
+        self._children.pop()
 
     def is_composite(self) -> bool:
         return True
 
-    def apply(self, image):
+    def get_filter_list(self) -> str:
+        f_list = str()
+        if len(self._children) != 0:
+            for child in self._children:
+                f_list += str(child.get_filter_list() + "\n")
+            return f_list
+        else:
+            return ""
+
+    def apply(self, image) -> Image:
         self.image = image
         for child in self._children:
             self.image = child.apply(self.image)
         return self.image
 
 
-mng = Manager()
+MNG = Manager()
 print("opening file")
-mng.open_file("test.png")
-print("add filter to chain")
-mng.add_filter_to_chain("blur", 3)
-print("add filter to chain")
-mng.add_filter_to_chain("blur", 3)
-print("add filter to chain")
-mng.add_filter_to_chain("blur", 3)
+MNG.open_file("test.png")
+
+
+
+# print("add filter to chain")
+# mng.add_filter_to_chain("rotate", 30)
+# print("add filter to chain")
+MNG.add_filter_to_chain("resize", 700, 700)
+# MNG.add_filter_to_chain("flip", "v")
+# MNG.add_filter_to_chain("flip", "h")
+# MNG.add_filter_to_chain("color", 1.5)
+# MNG.add_filter_to_chain("contrast", 1.5)
+# MNG.add_filter_to_chain("brightness", 1.5)
+MNG.add_filter_to_chain("sharpness", 30)
+MNG.add_filter_to_chain("blur", 3)
+
+
+MNG.save_complex_filter()
+
+print("load_filters")
+# MNG.load_complex_filter("filter.txt")
+
+# MNG.remove_filter_from_chain()
+
+# MNG.add_filter_to_chain("boxblur", 3)
+
 print("apply filter")
-mng.apply_filter_chain()
+MNG.apply_filter_chain()
+
 print("save file")
-mng.save_file()
+MNG.save_file("out.jpg")
